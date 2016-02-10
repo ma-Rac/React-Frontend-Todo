db0b311950583f788a595c562f377bfbb70557f9
rorykoehein <rory@intelworks.com>
2016-02-10 21:00:04 +0100

    Simplify

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import React, { Component } from 'react';
-import { NICE, SUPER_NICE } from './colors';
+import React from 'react';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
-    );
-  }
+class App extends React.Component {
+    render() {
+        return (
+            <h1>Hello World!</h1>
+        );
+    }
 }
 
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
-      </div>
-    );
-  }
-}
+export default App;
